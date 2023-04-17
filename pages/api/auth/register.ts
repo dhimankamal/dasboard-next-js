2bc3a1271e12b3288dd9a8d3a8a1ad55994b44b4
kamal <dhimank079@gmail.com>
2023-04-17 07:39:04 +0530

    sign up user

---
+++ pages/api/auth/register.ts
@@ -0,0 +1,34 @@
+import { NextApiHandler } from "next";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { prisma } from "@/lib/db";
+
+const JWT_SECRET = "test";
+
+const register: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
+  }
+
+  const { email, password } = req.body;
+
+  console.log("email>>>>", email)
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name: "kamal",
+      },
+    });
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+    res.status(200).json({ token });
+  } catch (err:any) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export default register;
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,10 +1,33 @@
 import { Button } from "@/components/ui/Button";
 import Input from "@/components/ui/Input";
+import axios from "axios";
 import { NextPage } from "next";
+import { useState } from "react";
 
 interface Props {}
 
 const Signup: NextPage<Props> = ({}) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const res = await  axios.post('/api/auth/register',formData);
+      console.log("res", res);
+    } catch (error) {
+      console.log("error", error)
+    }
+    console.log("formData", formData);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({ ...prevState, [name]: value }));
+  };
   return (
     <div className="container mx-auto py-12">
       <h1>Sign up</h1>
@@ -12,18 +35,31 @@ const Signup: NextPage<Props> = ({}) => {
         This information will be displayed publicly so be careful what you
         share.
       </p>
-      <form className="py-6 space-y-4 w-6/12">
+      <form className="py-6 space-y-4 w-6/12" onSubmit={handleSignUp}>
         <Input
           label="Full name"
-          inputProps={{ placeholder: "Enter your full name" }}
+          inputProps={{
+            placeholder: "Enter your full name",
+            name: "name",
+            onChange: handleInputChange,
+          }}
         />
         <Input
           label="Email address"
-          inputProps={{ placeholder: "Enter your email" }}
+          inputProps={{
+            placeholder: "Enter your email",
+            name: "email",
+            onChange: handleInputChange,
+          }}
         />
         <Input
           label="Password"
-          inputProps={{ placeholder: "Enter your password", type: "password" }}
+          inputProps={{
+            placeholder: "Enter your password",
+            type: "password",
+            name: "password",
+            onChange: handleInputChange,
+          }}
         />
         <Input
           label="Confirm password"
